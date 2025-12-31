ec5481d821c22cdf0f66cd0302b7fd8368b9430c
Aisha Junaid <ayeshajunaidmemon@gmail.com>
2025-12-31 16:24:28 +0500

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -20,7 +20,7 @@ const config = {
   },
 
   // Set the production url of your site here
-  url: 'https://ai-book-project.github.io',
+  url: 'https://ai-book-hackathon-wheat.vercel.app/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
